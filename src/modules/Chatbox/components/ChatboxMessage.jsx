7ca185987e075ac09a4fd3720a3e791feb63cf26
Hayden Carlson <hayden.carlson@icloud.com>
2018-04-04 20:26:01 -0700

    Style chatbox messages. Add email to chat message

--- src/modules/Chatbox/components/ChatboxMessage.jsx
+++ src/modules/Chatbox/components/ChatboxMessage.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 class ChatboxMessage extends React.Component {
   render () {
     return (
-      <li className="chat-message">{this.props.content}</li>
+      <li className="chat-message-container">
+        <div class="chat-message-text">
+          {this.props.content}
+        </div>
+    </li>
     )
   }
 }
