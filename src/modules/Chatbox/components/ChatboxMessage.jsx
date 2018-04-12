b211c59661e89d74ed52c32564861e69f51f5c40
Hayden Carlson <hayden.carlson@icloud.com>
2018-04-11 21:33:29 -0700

    Add avatars to chatbox

--- src/modules/Chatbox/components/ChatboxMessage.jsx
+++ src/modules/Chatbox/components/ChatboxMessage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import defaultAvatar from '../images/default_avatar.png';
 class ChatboxMessage extends React.Component {
   render () {
     return (
       <li className="chat-message-container">
+        <img src={defaultAvatar} className="chat-message-avatar"/>
         <div class="chat-message-text">
           <span>{this.props.username}</span>
-          {this.props.content}
+          <span>{this.props.content}</span>
         </div>
     </li>
     )
